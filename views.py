86165a3f6f1a68b03466370f293b61700aef1018
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2022-08-01 13:16:05 +0100

    implemented possibility of resolution change

--- views.py
+++ views.py
@@ -1,7 +1,14 @@
 import imageio.v3 as imageio
+from skimage.transform import resize
+import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import filedialog
 from logic import *
+
+def show_image(img):
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
 
 root = tk.Tk()
 root.withdraw() # if there isnt any other gui element
@@ -9,32 +16,31 @@ root.withdraw() # if there isnt any other gui element
 file_path = filedialog.askopenfilename()
 
 img = imageio.imread(file_path)
+show_image(img)
+
+height = img.shape[0]
+width = img.shape[1]
+
+jump_value = 10
+
+img = resize(img, (height / jump_value, width / jump_value))
+show_image(img)
 
 height = img.shape[0]
 width = img.shape[1]
 
 file = open("output.txt", "a")
 
-jump_value = 2
-
-y = 0
-while y < height:
+for y in range(height):
     line = ""
-    x = 0
-    while x < width:
-        grayscales = []
-        for _ in range(jump_value):
-            if x >= width:
-                break
-            pixel = img[y, x]
-            r = int(pixel[0])
-            g = int(pixel[1])
-            b = int(pixel[2])
-            grayscales.append(get_grayscale(r, g, b))
-            x = x + 1
-        line += get_char(average(grayscales))
+    for x in range(width):
+        pixel = img[y, x]
+        r = int(pixel[0] * 255)
+        g = int(pixel[1] * 255)
+        b = int(pixel[2] * 255)
+        grayscale = get_grayscale(r, g, b)
+        line += get_char(grayscale)
     print(line)
     file.write(line)
-    y = y + jump_value
 
 file.close()
