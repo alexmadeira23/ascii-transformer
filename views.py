14e5bcc997a1a96f33b3e0b118f35f1fcd6667ab
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2022-08-01 16:50:02 +0100

    gui partially done

--- views.py
+++ views.py
@@ -1,95 +1,90 @@
-import imageio.v3 as imageio
-from skimage.transform import resize
-import matplotlib.pyplot as plt
-import tkinter as tk
+from PIL import Image, ImageTk
 from tkinter import filedialog
 import PySimpleGUI as sg
 from logic import *
 
-def show_image(img):
-    plt.imshow(img)
-    plt.axis("off")
-    plt.show()
+sg.theme('DarkGrey13') 
 
 def app():
-    original_img = None
+    image = None
     resolution_decrease = 1
 
-    # Define the window's contents
     layout = [
-        [sg.Button('Choose photo')],
-        [sg.Text("Resolution decrease value: ")],
-        [sg.Slider(range=(1, 10), orientation="h", key='-SLIDER-')],
-        [sg.Image(key="-IMAGE-")],
-        [sg.Button('Transform into ASCII art'), sg.Button('Quit')],
-        [sg.Text(size=(40,1), key='-OUTPUT-')],
+        [sg.Button('Choose photo'),
+        [sg.Text("Resolution decrease: ", justification="center"), sg.Slider(range=(1, 100), expand_x=True, orientation="h", key='-SLIDER-')],
+        [sg.Image(size=(300, 300), key="-IMAGE-"), sg.Multiline(size=(100, 30), disabled=True, background_color="black", text_color="white", font="consolas", key='-OUTPUT-')]],
+        [sg.Button('Transform'), sg.Button('Quit')]
     ]
 
-    # Create the window
     window = sg.Window('Ascii Transformer', layout, finalize=True)
-
     window['-SLIDER-'].bind('<ButtonRelease-1>', ' Release')
 
-    # Display and interact with the Window using an Event Loop
     while True:
         event, values = window.read()
 
-        # See if user wants to quit or window was closed
         if event == sg.WINDOW_CLOSED or event == 'Quit':
             break
 
         elif event == "Choose photo":
             file_path = filedialog.askopenfilename()
             if file_path != "":
-                original_img = imageio.imread(file_path)
-                window["-IMAGE-"].update(source=file_path)
+                image = Image.open(file_path)
+                image = image.resize(image.size, resample=Image.Resampling.BICUBIC)
+                photo_image = ImageTk.PhotoImage(image=image)
+                window["-IMAGE-"].update(data=photo_image)
 
         elif event == '-SLIDER- Release':
+            height = image.size[1]
+            width = image.size[0]
             resolution_decrease = values['-SLIDER-']
+            img = image.resize((math.floor(width / resolution_decrease), math.floor(height / resolution_decrease)), resample=Image.Resampling.BICUBIC)
+            photo_img = ImageTk.PhotoImage(image=img)
+            window["-IMAGE-"].update(data=photo_img)
 
-        elif event == 'Transform into ASCII art':
-            height = original_img.shape[0]
-            width = original_img.shape[1]
+        elif event == 'Transform':
+            height = image.size[1]
+            width = image.size[0]
+            img = image.resize((math.floor(width / resolution_decrease), math.floor(height / resolution_decrease)), resample=Image.Resampling.BICUBIC)
+            output = window["-OUTPUT-"]
+            output.update("")
+            print_ascii(img, output)
 
-            img = resize(original_img, (height / resolution_decrease, width / resolution_decrease))
-
-            print_ascii(img)
-
-
-        # Output a message to the window
-        #window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
-
-    # Finish up by removing from the screen
     window.close()
 
 def console_app():
     file_path = filedialog.askopenfilename()
-    img = imageio.imread(file_path)
-    show_image(img)
+    img = Image.open(file_path)
+    img.show()
 
-    height = img.shape[0]
-    width = img.shape[1]
+    height = img.size[1]
+    width = img.size[0]
 
     resolution_decrease = 2
 
-    img = resize(img, (height / resolution_decrease, width / resolution_decrease))
-    show_image(img)
+    img = img.resize((math.floor(width / resolution_decrease), math.floor(height / resolution_decrease)), resample=Image.Resampling.BICUBIC)
+    img.show()
 
     print_ascii(img)
 
-def print_ascii(img):
-    height = img.shape[0]
-    width = img.shape[1]
+def print_ascii(img, output=None):
+    height = img.size[1]
+    width = img.size[0]
+
+    px = img.load()
 
     for y in range(height):
         line = ""
         for x in range(width):
-            pixel = img[y, x]
-            r = int(pixel[0] * 255)
-            g = int(pixel[1] * 255)
-            b = int(pixel[2] * 255)
+            pixel = px[x, y]
+            r, g, b = pixel[0], pixel[1], pixel[2]
             grayscale = get_grayscale(r, g, b)
             line += get_char(grayscale)
-        print(line)
+        if output != None:
+            if output.get() == "":
+                output.update(line)
+            else:
+                output.update(output.get() + "\n" + line)
+        else:
+            print(line)
 
 app()
