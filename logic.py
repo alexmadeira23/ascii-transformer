dd990de2130e0c082216401a8cc31aa499de9d5c
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2022-08-06 20:18:02 +0100

    improved structure

---
+++ logic.py
@@ -0,0 +1,47 @@
+import math
+
+BLACK_BACKGROUND = True
+
+#CHARS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+CHARS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'.                           "
+
+CHARS_SIZE = len(CHARS)
+MAX_INTENSITY = 255
+
+RED_CONTRIBUTION = 0.3
+GREEN_CONTRIBUTION = 0.59
+BLUE_CONTRIBUTION = 0.11
+
+if BLACK_BACKGROUND:
+    CHARS = CHARS[::-1]
+
+def get_grayscale(r, g, b):
+    return RED_CONTRIBUTION * r + GREEN_CONTRIBUTION * g + BLUE_CONTRIBUTION * b
+
+def get_char(v):
+    index = math.floor(v * (CHARS_SIZE - 1) / MAX_INTENSITY)
+    return CHARS[index]
+
+def image_to_text(img):
+    text = ""
+
+    width = img.size[0]
+    height = img.size[1]
+
+    px = img.load()
+
+    for y in range(height):
+        line = ""
+        
+        for x in range(width):
+            pixel = px[x, y]
+            r, g, b = pixel[0], pixel[1], pixel[2]
+            grayscale = get_grayscale(r, g, b)
+            line += get_char(grayscale)
+
+        if text == "":
+            text = text + line
+        else:
+            text = text + "\n" + line
+    
+    return text
--- view.py
+++ view.py
@@ -3,18 +3,49 @@ from tkinter import filedialog
 import PySimpleGUI as sg
 import cv2
 from threading import Thread
-from utils import *
+from logic import *
 
 sg.theme('GrayGrayGray') 
 
-res_decrease = 1
-webcam_on = False
-
 def app():
-    current_image = None
+
+    def choose_image_action():
+        global current_image
+        file_path = filedialog.askopenfilename()
+        if file_path != "":
+            current_image = Image.open(file_path)
+            thumbnail = current_image.copy()
+            thumbnail.thumbnail((200, 1000), resample=Image.Resampling.BICUBIC)
+            photo_image = ImageTk.PhotoImage(image=thumbnail)
+            window["-IMAGE-"].update(data=photo_image)
+
+    def slider_releas_action():
+        global res_decrease
+        res_decrease = values['-SLIDER-']
+
+    def transform_image_action():
+        global current_image
+        global res_decrease
+        if current_image != None:
+                copy = current_image.copy()
+                copy.thumbnail(get_new_size(current_image.size, res_decrease), resample=Image.Resampling.BICUBIC)
+                text = image_to_text(copy)
+                window["-OUTPUT-"].update(text)
+        else:
+            sg.popup("You need to select an image first!", no_titlebar=True, button_type=5, auto_close=True, auto_close_duration=1)
+
+    def webcam_action():
+        global webcam_on
+        if not webcam_on:
+                webcam_on = True
+                Thread(target=lambda : webcam_job(window["-OUTPUT-"], show_video=False), daemon=True).start()
+        else:
+            webcam_on = False
 
     global res_decrease
     global webcam_on
+    res_decrease = 1
+    webcam_on = False
 
     layout = [
         [sg.Text("Resolution decrease:", pad=((0, 0), (15, 0)), justification="center"), sg.Slider(range=(1, 100), expand_x=True, orientation="h", key='-SLIDER-')],
@@ -34,66 +65,23 @@ def app():
             break
 
         elif event == "Choose image":
-            file_path = filedialog.askopenfilename()
-            if file_path != "":
-                current_image = Image.open(file_path)
-                thumbnail = current_image.copy()
-                thumbnail.thumbnail((200, 1000), resample=Image.Resampling.BICUBIC)
-                photo_image = ImageTk.PhotoImage(image=thumbnail)
-                window["-IMAGE-"].update(data=photo_image)
+            choose_image_action()
 
         elif event == '-SLIDER- Release':
-            res_decrease = values['-SLIDER-']
+            slider_releas_action()
 
         elif event == 'Transform image':
-            if current_image != None:
-                copy = current_image.copy()
-                copy.thumbnail(get_new_size(current_image.size, res_decrease), resample=Image.Resampling.BICUBIC)
-                print_ascii(copy, window["-OUTPUT-"])
-            else:
-                sg.popup("You need to select an image first!", no_titlebar=True, button_type=5, auto_close=True, auto_close_duration=1)
+            transform_image_action()
         
         elif event == "Webcam":
-            if not webcam_on:
-                webcam_on = True
-                Thread(target=lambda : webcam(window["-OUTPUT-"], show_video=False), daemon=True).start()
-            else:
-                webcam_on = False
+            webcam_action()
 
     window.close()
 
-def print_ascii(img, output=None):
-    text = ""
-
-    width = img.size[0]
-    height = img.size[1]
-
-    px = img.load()
-
-    for y in range(height):
-        line = ""
-        
-        for x in range(width):
-            pixel = px[x, y]
-            r, g, b = pixel[0], pixel[1], pixel[2]
-            grayscale = get_grayscale(r, g, b)
-            line += get_char(grayscale)
-
-        if text == "":
-            text = text + line
-        else:
-            text = text + "\n" + line
-        
-    if output != None:
-        output.update(text)
-    else:
-        print(text)
-
-def webcam(output=None, show_video=False):
+def webcam_job(output=None, show_video=False):
     global res_decrease
     global webcam_on
     cam = cv2.VideoCapture(0)
-    print(vars(output))
 
     while webcam_on:
         ret, frame = cam.read()
@@ -109,12 +97,16 @@ def webcam(output=None, show_video=False):
         
         image = Image.fromarray(flipped)
         image.thumbnail(get_new_size(image.size, res_decrease), resample=Image.Resampling.BICUBIC)
-        print_ascii(image, output)
+        text = image_to_text(image)
+        output.update(text)
 
         cv2.waitKey(1)
 
     cam.release()
     cv2.destroyAllWindows()
+
+def get_new_size(size, res_decrease):
+    return (math.floor(size[0] / res_decrease), math.floor(size[1] / res_decrease))
 
 def main():
     app()
